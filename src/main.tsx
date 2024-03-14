b10bdcdc36c489efc49d6ada047a5908ed9e7ffa
jasmine0714 <woooooooow22@gmail.com>
2024-03-14 21:16:29 +0900

    ✨ RouterProvider 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
+import router from './router.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={router} />
+		<App />
+	</React.StrictMode>,
+);

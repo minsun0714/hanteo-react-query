614e2d996f77378bdfa02fd0914b0f0b24de7ef7
jasmine0714 <woooooooow22@gmail.com>
2024-03-14 20:13:05 +0900

    🔧 기존 eslint import 설정을 prettier import 자동 정렬로 대신함

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,45 +1,23 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "plugin:import/typescript",
-    "plugin:import/recommended",
-    "plugin:react-hooks/recommended",
-  ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "./tsconfig.json",
-  },
-  plugins: ["react-refresh"],
-  settings: {
-    "import/resolver": {
-      node: {},
-      typescript: {
-        directory: "./src",
-      },
-    },
-    "import/parsers": { "@typescript-eslint/parser": [".ts", ".tsx"] },
-  },
-  rules: {
-    "import/order": [
-      "error",
-      {
-        groups: ["builtin", "external", "parent", "sibling", "index"],
-        alphabetize: {
-          order: "asc",
-          caseInsensitive: true,
-        },
-        "newlines-between": "never",
-      },
-    ],
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-    "@typescript-eslint/no-explicit-any": "warn",
-  },
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier',
+		'plugin:react-hooks/recommended',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: './tsconfig.json',
+	},
+	plugins: ['react-refresh'],
+	rules: {
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
+		],
+		'@typescript-eslint/no-explicit-any': 'warn',
+	},
 };

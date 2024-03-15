428171a1f60a33521d9f9394ad566e280ac9af8d
jasmine0714 <woooooooow22@gmail.com>
2024-03-15 17:28:53 +0900

    🚧 form schema에 정규식 및 에러 메시지 추가

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -2,11 +2,17 @@ import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import * as z from 'zod';
+import { ErrorMessage } from '@hookform/error-message';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 const schema = z.object({
 	id: z.string().email({ message: '이메일 형식으로 입력해주세요' }),
-	pw: z.string().max(13, { message: '비밀번호는 13자 이하로 입력해주세요' }),
+	pw: z
+		.string()
+		.max(13, { message: '비밀번호는 13자 이하로 입력해주세요' })
+		.regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).+$/, {
+			message: '비밀번호는 숫자, 영문-소, 영문-대, 특수문자를 포함해야 합니다.',
+		}),
 });
 
 const LoginPage = () => {
@@ -22,10 +28,14 @@ const LoginPage = () => {
 
 	return (
 		<form onSubmit={handleSubmit(onSubmit)}>
-			<Input {...register('id')} placeholder="ID를 입력해주세요" />
-			{errors.id && <p>{errors.id.message as string}</p>}
-			<Input {...register('pw')} placeholder="PW를 입력해주세요" />
-			{errors.pw && <p>{errors.pw.message as string}</p>}
+			<div>
+				<Input {...register('id')} placeholder="ID를 입력해주세요" />
+				<ErrorMessage errors={errors} name="id" />
+			</div>
+			<div>
+				<Input {...register('pw')} placeholder="PW를 입력해주세요" />
+				<ErrorMessage errors={errors} name="pw" />
+			</div>
 			<div>
 				<Button text="로그인" />
 				<Button text="로그인" />

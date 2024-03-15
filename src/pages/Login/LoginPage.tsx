97c06ef2458d4405d396f175b819edbf5ef7306f
jasmine0714 <woooooooow22@gmail.com>
2024-03-15 15:35:18 +0900

    ✨ 로그인 페이지 뷰 생성

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,5 +1,17 @@
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
 const LoginPage = () => {
-	return <div>로그인 페이지</div>;
+	return (
+		<>
+			<Input placeholder="ID를 입력해주세요" />
+			<Input placeholder="PW를 입력해주세요" />
+			<div>
+				<Button text="로그인" />
+				<Button text="로그인" />
+			</div>
+		</>
+	);
 };
 
 export default LoginPage;

ca86c8b6cd1db0fc5a429c8feb9fa9d3c6ec350c
jasmine0714 <woooooooow22@gmail.com>
2024-03-15 14:31:05 +0900

    💄 이미지 업로드 버튼일 경우의 조건부 스타일링 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,15 @@
 type ButtonProps = {
 	text: string;
 	onClick?: () => void;
+	imgUpload?: boolean;
 };
 
-const Button = ({ text, onClick }: ButtonProps) => {
-	return <button onClick={onClick}>{text}</button>;
+const Button = ({ text, onClick, imgUpload }: ButtonProps) => {
+	return (
+		<button onClick={onClick} className={imgUpload ? 'img-upload' : ''}>
+			{text}
+		</button>
+	);
 };
 
 export default Button;

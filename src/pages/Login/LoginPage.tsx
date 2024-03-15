0a59838856b03e1dbbc471524b6eaf934e00b320
jasmine0714 <woooooooow22@gmail.com>
2024-03-15 17:51:56 +0900

    🚧 필수 입력 사항 에러메시지 추가

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -6,9 +6,13 @@ import { ErrorMessage } from '@hookform/error-message';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 const formSchema = z.object({
-	id: z.string().email({ message: '이메일 형식으로 입력해주세요' }),
+	id: z
+		.string()
+		.min(1, { message: '아이디를 입력해주세요' })
+		.email({ message: '이메일 형식으로 입력해주세요' }),
 	pw: z
 		.string()
+		.min(1, { message: '비밀번호를 입력해주세요' })
 		.max(13, { message: '비밀번호는 13자 이하로 입력해주세요' })
 		.regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).+$/, {
 			message: '비밀번호는 숫자, 영문-소, 영문-대, 특수문자를 포함해야 합니다.',

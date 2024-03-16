6621680974c6cfe353bcde1fab0fe39c0d44383e
jasmine0714 <woooooooow22@gmail.com>
2024-03-16 16:50:33 +0900

    ✨ refine 메서드를 통해 비밀번호 != 비밀번호 확인 여부 유효성 검사 추가

--- src/pages/SignUp/SignUpPage.tsx
+++ src/pages/SignUp/SignUpPage.tsx
@@ -7,24 +7,32 @@ import { ErrorMessage } from '@hookform/error-message';
 import { zodResolver } from '@hookform/resolvers/zod';
 import DefaultImg from '../../assets/default.svg';
 
-const formSchema = z.object({
-	id: z
-		.string()
-		.min(1, { message: '아이디를 입력해주세요' })
-		.email({ message: '이메일 형식으로 입력해주세요' }),
-	pw: z
-		.string()
-		.min(1, { message: '비밀번호를 입력해주세요' })
-		.max(13, { message: '비밀번호는 13자 이하로 입력해주세요' })
-		.regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).+$/, {
-			message: '비밀번호는 숫자, 영문-소, 영문-대, 특수문자를 포함해야 합니다.',
-		}),
-	pwConfirm: z.string().min(1, { message: '비밀번호 확인을 입력해주세요' }),
-	name: z
-		.string()
-		.min(1, { message: '이름을 입력해주세요' })
-		.max(5, { message: '이름은 5자 이하로 입력해주세요' }),
-});
+const formSchema = z
+	.object({
+		id: z
+			.string()
+			.min(1, { message: '아이디를 입력해주세요' })
+			.email({ message: '이메일 형식으로 입력해주세요' }),
+		pw: z
+			.string()
+			.min(1, { message: '비밀번호를 입력해주세요' })
+			.max(13, { message: '비밀번호는 13자 이하로 입력해주세요' })
+			.regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).+$/, {
+				message:
+					'비밀번호는 숫자, 영문-소, 영문-대, 특수문자를 포함해야 합니다.',
+			}),
+		pwConfirm: z.string().min(1, { message: '비밀번호 확인을 입력해주세요' }),
+		name: z
+			.string()
+			.min(1, { message: '이름을 입력해주세요' })
+			.max(5, { message: '이름은 5자 이하로 입력해주세요' }),
+	})
+	.refine(
+		(data) => {
+			return data.pw === data.pwConfirm;
+		},
+		{ message: '비밀번호가 일치하지 않습니다.', path: ['pwConfirm'] },
+	);
 
 const SignUpPage = () => {
 	// const [fileName, setFileName] = useState('');

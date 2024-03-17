2dc963281d83627216e8932343ebafa5ba263db1
jasmine0714 <woooooooow22@gmail.com>
2024-03-17 14:21:09 +0900

    ➕ eslint plugin query 사용 시도

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,18 +6,23 @@ module.exports = {
 		'plugin:@typescript-eslint/recommended',
 		'prettier',
 		'plugin:react-hooks/recommended',
+		'plugin:@tanstack/eslint-plugin-query/recommended',
 	],
 	ignorePatterns: ['dist', '.eslintrc.cjs'],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		project: './tsconfig.json',
 	},
-	plugins: ['react-refresh'],
+	plugins: ['react-refresh', '@tanstack/query'],
 	rules: {
 		'react-refresh/only-export-components': [
 			'warn',
 			{ allowConstantExport: true },
 		],
 		'@typescript-eslint/no-explicit-any': 'warn',
+		'@tanstack/query/exhaustive-deps': 'error',
+		'@tanstack/query/no-deprecated-options': 'error',
+		'@tanstack/query/prefer-query-object-syntax': 'error',
+		'@tanstack/query/stable-query-client': 'error',
 	},
 };

50bcef1f636bba258a06cfdbc24316a791f0751b
jasmine0714 <woooooooow22@gmail.com>
2024-03-17 19:03:44 +0900

    ✨ 아이디 비밀번호 일치 여부 확인

--- src/pages/Login/components/LoginForm.tsx
+++ src/pages/Login/components/LoginForm.tsx
@@ -1,15 +1,39 @@
 import { FieldValues, SubmitHandler, useFormContext } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 
 type LoginFormProps = {
 	children: React.ReactNode;
 };
 
+function getCookie(key: string) {
+	const cookieValue = `; ${document.cookie}`;
+	const cookieArr = cookieValue
+		.split('; ')
+		.map((keyValue: string) => keyValue.split('='));
+	return cookieArr.find(([cookieKey]) => cookieKey === key)?.[1];
+}
+
 const LoginForm = ({ children }: LoginFormProps) => {
+	const navigate = useNavigate();
 	const { handleSubmit } = useFormContext();
 
-	const onSubmit: SubmitHandler<FieldValues> = (data) => {
-		console.log(data);
+	const onSubmit: SubmitHandler<FieldValues> = (formFieldData) => {
+		const cookieData = {
+			id: getCookie('id'),
+			pw: getCookie('pw'),
+		};
+
+		if (
+			formFieldData.id !== cookieData.id ||
+			formFieldData.pw !== cookieData.pw
+		) {
+			alert('아이디가 일치하지 않습니다.');
+			return;
+		}
+
+		navigate('/myinfo');
 	};
+
 	return <form onSubmit={handleSubmit(onSubmit)}>{children}</form>;
 };
 

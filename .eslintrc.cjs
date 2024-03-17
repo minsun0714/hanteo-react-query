2b10bf582aee9e496d995399719a63a9d61d2457
jasmine0714 <woooooooow22@gmail.com>
2024-03-17 14:35:54 +0900

    Revert "➕ eslint plugin query 사용 시도"
    This reverts commit 2dc963281d83627216e8932343ebafa5ba263db1.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,23 +6,18 @@ module.exports = {
 		'plugin:@typescript-eslint/recommended',
 		'prettier',
 		'plugin:react-hooks/recommended',
-		'plugin:@tanstack/eslint-plugin-query/recommended',
 	],
 	ignorePatterns: ['dist', '.eslintrc.cjs'],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		project: './tsconfig.json',
 	},
-	plugins: ['react-refresh', '@tanstack/query'],
+	plugins: ['react-refresh'],
 	rules: {
 		'react-refresh/only-export-components': [
 			'warn',
 			{ allowConstantExport: true },
 		],
 		'@typescript-eslint/no-explicit-any': 'warn',
-		'@tanstack/query/exhaustive-deps': 'error',
-		'@tanstack/query/no-deprecated-options': 'error',
-		'@tanstack/query/prefer-query-object-syntax': 'error',
-		'@tanstack/query/stable-query-client': 'error',
 	},
 };

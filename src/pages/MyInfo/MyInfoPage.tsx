f42c6ec0967661a98bfaeb2b324f87ab3c76494f
jasmine0714 <woooooooow22@gmail.com>
2024-03-17 22:40:18 +0900

    🏷️ getMyInfo 함수의 return type 지정

--- src/pages/MyInfo/MyInfoPage.tsx
+++ src/pages/MyInfo/MyInfoPage.tsx
@@ -2,33 +2,25 @@ import { FormProvider, get, useForm } from 'react-hook-form';
 import Button from '../../components/Button';
 import ImageUpload from '../../components/ImageUpload';
 import Input from '../../components/Input';
+import { AuthService } from '../../service/AuthService';
 import { getCookie } from '../../util/function/getCookie';
 import MyInfoForm from './components/MyInfoForm';
 import { formSchema } from './formSchema';
-import axios from 'axios';
 import { ErrorMessage } from '@hookform/error-message';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useQuery } from '@tanstack/react-query';
+
+const authService = new AuthService();
+const { data } = authService.useGetMyInfoQuery();
 
 const MyInfoPage = () => {
-	const { data } = useQuery({
-		queryKey: ['myInfo'],
-		queryFn: async () => {
-			const response = await axios.get(
-				'http://localhost:4000/api/auth/my-info',
-			);
-			return response.data;
-		},
-	});
-
 	const form = useForm({
 		resolver: zodResolver(formSchema),
 		defaultValues: {
-			profileImage: data.profileImage || get('profileImage') || '-',
-			id: data.id || getCookie('id') || '-',
-			name: data.name || getCookie('name') || '-',
-			createdAt: data.createdAt || getCookie('createdAt') || '-',
-			updatedAt: data.updatedAt || getCookie('updatedAt') || '-',
+			profileImage: data?.profileImage || get('profileImage') || '-',
+			id: data?.id || getCookie('id') || '-',
+			name: data?.name || getCookie('name') || '-',
+			createdAt: data?.createdAt || getCookie('createdAt') || '-',
+			updatedAt: data?.updatedAt || getCookie('updatedAt') || '-',
 		},
 	});
 
--- src/service/AuthService.ts
+++ src/service/AuthService.ts
@@ -1,9 +1,21 @@
 import { FieldValues } from 'react-hook-form';
 import axios, { AxiosError } from 'axios';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
+
+interface IMyInfo {
+	id: string;
+	name: string;
+	createdAt: string;
+	updatedAt: string;
+	profileImage: string;
+}
 
 export class AuthService {
 	URL = 'http://localhost:4000';
+
+	private getMyInfo = async (): Promise<IMyInfo> => {
+		return axios.get(`${this.URL}/api/auth/my-info`);
+	};
 
 	private postSignUp = async (payload: FieldValues) => {
 		return axios.post(`${this.URL}/api/auth/sign-up`, payload);
@@ -30,6 +42,12 @@ export class AuthService {
 			}
 			alert('알 수 없는 오류가 발생했습니다. 다시 시도해주세요.');
 		},
+	};
+	public useGetMyInfoQuery = () => {
+		return useQuery({
+			queryKey: ['myInfo'],
+			queryFn: this.getMyInfo,
+		});
 	};
 
 	public useSignUpMutation = () => {

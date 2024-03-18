a5f70467d157b3f12e0f75005960221d5b53368d
jasmine0714 <woooooooow22@gmail.com>
2024-03-18 15:50:31 +0900

    ✨ 지원하지 않는 해상도의 경우 에러 로그 출력

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { Outlet, useMatches } from 'react-router-dom';
 import { PATH_NAME } from './util/constants/path';
 
@@ -10,6 +10,20 @@ const Layout = () => {
 			| undefined;
 		return match ? PATH_NAME[match] : '페이지를 찾을 수 없습니다.';
 	}, [matches]);
+
+	useEffect(() => {
+		const handleResize = () => {
+			if (window.innerWidth > 770 || window.innerWidth < 320) {
+				throw new Error('지원하지 않는 해상도입니다.');
+			}
+		};
+
+		window.addEventListener('resize', handleResize);
+
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
 
 	return (
 		<>
